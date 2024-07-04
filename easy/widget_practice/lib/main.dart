26f61c5a186a8d14d569ffb5e15cd736199a26b9
fivezero2494 <fivezero2494@naver.com>
2024-07-04 21:28:30 +0900

    lec 2 시작

---
+++ easy/widget_practice/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Charactor card',
+      home: MyCard(),
+    );
+  }
+}
+
+class MyCard extends StatelessWidget {
+  const MyCard({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BBANTO'),
+      ),
+    );
+  }
+}
