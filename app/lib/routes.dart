0409a09736cd2f2a29214749a8cfb728c1ca9203
SeoJun <jun@SeoJunui-MacBookPro.local>
2024-07-05 19:43:12 +0900

    feat : Route 세팅 추가

---
+++ app/lib/routes.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/profile_screen.dart';
+import 'screens/flower_screen.dart';
+import 'screens/activity_input_screen.dart';
+
+class Routes {
+  static final Map<String, WidgetBuilder> routes = {
+    '/home': (context) => HomeScreen(),
+    '/login': (context) => LoginScreen(),
+    '/profile': (context) => ProfileScreen(),
+    '/flower': (context) => FlowerScreen(),
+    '/activity_input': (context) => ActivityInputScreen(),
+  };
+
+  static Route<dynamic>? generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/home':
+        return MaterialPageRoute(builder: (context) => HomeScreen());
+      case '/login':
+        return MaterialPageRoute(builder: (context) => LoginScreen());
+      case '/profile':
+        return MaterialPageRoute(builder: (context) => ProfileScreen());
+      case '/flower':
+        return MaterialPageRoute(builder: (context) => FlowerScreen());
+      case '/activity_input':
+        return MaterialPageRoute(builder: (context) => ActivityInputScreen());
+      default:
+        return null; // null을 반환하여 알 수 없는 경로일 경우 오류 방지
+    }
+  }
+}
