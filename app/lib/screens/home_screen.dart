e6ccc9d4a70710c67a574fa713b5091d695a5618
SeoJun <jun@SeoJunui-MacBookPro.local>
2024-07-05 19:43:42 +0900

    feat : Home 스크린에 로그인 화면으로 이동하는 버튼 추가

---
+++ app/lib/screens/home_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Welcome to BlossomDays!'),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/login');
+              },
+              child: Text('Go to Login'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
