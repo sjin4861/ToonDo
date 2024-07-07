965dd03c3cec97e0cb83840ac43f907ac547e4c5
fivezero2494 <fivezero2494@naver.com>
2024-07-07 09:47:32 +0900

    add: lec 23

--- easy/push_named/lib/ScreenA.dart
+++ easy/push_named/lib/ScreenA.dart
@@ -7,19 +7,34 @@ class ScreenA extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.menu,
+              color: Colors.white,
+            )),
         backgroundColor: Colors.black,
         centerTitle: true,
         title: const Text(
           'Screen A',
           style: TextStyle(color: Colors.white),
         ),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.shopping_cart, color: Colors.white)),
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.settings, color: Colors.white)),
+        ],
       ),
       body: Center(
           child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           ElevatedButton(
-              style: ElevatedButton.styleFrom(backgroundColor: Colors.grey),
+              style:
+                  ElevatedButton.styleFrom(backgroundColor: Colors.grey[800]),
               child: const Text(
                 'Go to Screen B',
                 style: TextStyle(color: Colors.white),
@@ -29,7 +44,8 @@ class ScreenA extends StatelessWidget {
             height: 15.0,
           ),
           ElevatedButton(
-              style: ElevatedButton.styleFrom(backgroundColor: Colors.grey),
+              style:
+                  ElevatedButton.styleFrom(backgroundColor: Colors.grey[800]),
               child: const Text(
                 'Go to Screen C',
                 style: TextStyle(color: Colors.white),
@@ -37,6 +53,11 @@ class ScreenA extends StatelessWidget {
               onPressed: () {}),
         ],
       )),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+        ),
+      ),
     );
   }
 }
