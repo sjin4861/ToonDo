f9e188b89a7e822a7823a80fb7c278636b152cc8
SeoJun <jun@SeoJunui-MacBookPro.local>
2024-11-15 21:06:39 +0900

    fix : MVVM 디자인 패턴 적용 중

--- todo_with_alarm/lib/services/todo_service.dart
+++ todo_with_alarm/lib/services/todo_service.dart
@@ -4,18 +4,17 @@ import 'package:todo_with_alarm/models/todo.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
 
-enum FilterOption { all, goal, importance }
-
 class TodoService {
-  // 모든 투두 리스트를 저장하는 메서드
-  static Future<void> saveAllTodos(List<Todo> todos) async {
+  // 투두 저장 메서드
+  Future<void> saveTodoList(List<Todo> todos) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    List<String> todoStrings = todos.map((todo) => jsonEncode(todo.toJson())).toList();
+    List<String> todoStrings =
+        todos.map((todo) => jsonEncode(todo.toJson())).toList();
     await prefs.setStringList('all_todos', todoStrings);
   }
 
-  // 모든 투두 리스트를 불러오는 메서드
-  static Future<List<Todo>> loadAllTodos() async {
+  // 투두 로드 메서드
+  Future<List<Todo>> loadTodoList() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     List<String>? todoStrings = prefs.getStringList('all_todos');
     if (todoStrings != null) {
@@ -27,34 +26,36 @@ class TodoService {
     }
   }
 
-  // 선택된 날짜에 해당하는 투두 필터링 메서드
-  static List<Todo> filterTodosByDate(
-      List<Todo> todos, DateTime selectedDate) {
-    DateTime selectedDateOnly =
-        DateTime(selectedDate.year, selectedDate.month, selectedDate.day);
-
-    return todos.where((todo) {
-      DateTime todoStartDate = DateTime(
-          todo.startDate.year, todo.startDate.month, todo.startDate.day);
-      DateTime todoEndDate =
-          DateTime(todo.endDate.year, todo.endDate.month, todo.endDate.day);
-
-      return (todoStartDate.isBefore(selectedDateOnly) ||
-              todoStartDate.isAtSameMomentAs(selectedDateOnly)) &&
-          (todoEndDate.isAfter(selectedDateOnly) ||
-              todoEndDate.isAtSameMomentAs(selectedDateOnly));
-    }).toList();
+  Future<void> updateTodoStatus(String todoId, double status) async {
+    // 투두 리스트 로드
+    List<Todo> todos = await loadTodoList();
+    // 해당 투두 찾기
+    int index = todos.indexWhere((todo) => todo.id == todoId);
+    if (index != -1) {
+      todos[index].status = status;
+      // 투두 리스트 저장
+      await saveTodoList(todos);
+    }
   }
 
-  // 투두 필터링 메서드 (필터 옵션 적용)
-  static List<Todo> applyFilter(
-      List<Todo> todos, FilterOption selectedFilter, String? selectedGoalId) {
-    if (selectedFilter == FilterOption.goal && selectedGoalId != null) {
-      return todos.where((todo) => todo.goalId == selectedGoalId).toList();
-    } else if (selectedFilter == FilterOption.importance) {
-      return todos.where((todo) => todo.importance >= 3).toList();
-    } else {
-      return todos;
+  Future<void> deleteTodoById(String id) async {
+    // 투두 리스트 로드
+    List<Todo> todos = await loadTodoList();
+    // 해당 투두 삭제
+    todos.removeWhere((todo) => todo.id == id);
+    // 투두 리스트 저장
+    await saveTodoList(todos);
+  }
+
+  Future<void> updateTodoDates(Todo todo) async {
+    // 투두 리스트 로드
+    List<Todo> todos = await loadTodoList();
+    // 해당 투두 업데이트
+    int index = todos.indexWhere((t) => t.id == todo.id);
+    if (index != -1) {
+      todos[index] = todo;
+      // 투두 리스트 저장
+      await saveTodoList(todos);
     }
   }
 }
