d4a8217fc4851ad2a9219b8bb0fdc9a08f6c2b81
SeoJun <jun@SeoJunui-MacBookPro.local>
2024-11-17 17:27:36 +0900

    feat : 휴대전화가 등록되어있는지 검색하는 메소드 추가

--- todo_with_alarm/lib/services/auth_service.dart
+++ todo_with_alarm/lib/services/auth_service.dart
@@ -1,12 +1,10 @@
-
-
 import 'package:todo_with_alarm/models/user.dart';
 
 class AuthService {
   // 임시로 사용자 데이터를 저장하는 리스트
   final List<User> _users = [];
 
-  // 싱글톤 패턴 적용 (필요 시)
+  // 싱글톤 패턴 적용
   static final AuthService _instance = AuthService._internal();
   factory AuthService() => _instance;
   AuthService._internal();
@@ -23,16 +21,28 @@ class AuthService {
   }
 
   // 사용자 등록
-  void registerUser(User user) {
-    _users.add(user);
+  Future<void> registerUser(User user) async {
+    // 이미 등록된 사용자 확인
+    final existingUser = findUserByPhoneNumber(user.phoneNumber);
+    if (existingUser == null) {
+      _users.add(user);
+    } else {
+      throw Exception('이미 등록된 휴대폰 번호입니다.');
+    }
   }
 
   // 사용자 로그인
-  bool login(String phoneNumber, String password) {
+  Future<bool> login(String phoneNumber, String password) async {
     final user = findUserByPhoneNumber(phoneNumber);
     if (user != null && user.password == password) {
       return true;
     }
     return false;
+  }
+
+  // 휴대폰 번호가 등록되었는지 확인
+  Future<bool> isPhoneNumberRegistered(String phoneNumber) async {
+    final user = findUserByPhoneNumber(phoneNumber);
+    return user != null;
   }
 }
