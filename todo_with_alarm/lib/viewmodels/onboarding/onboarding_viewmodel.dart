7a814194a610becd1bffa79156e660ae516224b2
SeoJun <jun@SeoJunui-MacBookPro.local>
2024-11-18 16:35:47 +0900

    feat : 닉네임 업데이트 백엔드와 연계

--- todo_with_alarm/lib/viewmodels/onboarding/onboarding_viewmodel.dart
+++ todo_with_alarm/lib/viewmodels/onboarding/onboarding_viewmodel.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import '../../services/auth_service.dart';
 
 class OnboardingViewModel extends ChangeNotifier {
   String nickname = '';
+  String userId;
 
-  void saveNickname() {
-    // 닉네임을 저장하는 로직 구현 (예: 서버에 저장하거나 로컬에 저장)
-    // ...
-    notifyListeners();
+  OnboardingViewModel({required this.userId});
+
+  Future<void> saveNickname() async {
+    final authService = AuthService();
+
+    try {
+      await authService.updateUsername(userId, nickname);
+    } catch (e) {
+      // 에러 처리
+      throw Exception('닉네임 업데이트에 실패했습니다: ${e.toString()}');
+    }
   }
 }
