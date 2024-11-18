4f8a2819c5540507230276e6e8fd0c41341b2c3b
SeoJun <jun@SeoJunui-MacBookPro.local>
2024-11-18 16:36:20 +0900

    feat : 로그인 없이 이용하기 구현

--- todo_with_alarm/lib/viewmodels/welcome/welcome_viewmodel.dart
+++ todo_with_alarm/lib/viewmodels/welcome/welcome_viewmodel.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todo_with_alarm/views/auth/signup_screen.dart';
+import 'package:todo_with_alarm/views/home/home_screen.dart';
 
 class WelcomeViewModel extends ChangeNotifier {
   // 소셜 로그인 처리 등을 위해 필요한 로직을 여기에 추가할 수 있습니다.
@@ -16,6 +17,14 @@ class WelcomeViewModel extends ChangeNotifier {
     Navigator.push(
       context,
       MaterialPageRoute(builder: (context) => SignupScreen()),
+    );
+  }
+
+  // "로그인 없이 이용하기" 메서드 추가
+  void continueWithoutLogin(BuildContext context) {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => HomeScreen()),
     );
   }
 }
