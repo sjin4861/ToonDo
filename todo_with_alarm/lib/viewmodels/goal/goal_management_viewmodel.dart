6ca802eb7787812eab035a276a7afc14d1719f66
SeoJun <jun@SeoJunui-MacBookPro.local>
2025-01-11 15:57:17 +0900

    feat : 필터링 로직 추가

--- todo_with_alarm/lib/viewmodels/goal/goal_management_viewmodel.dart
+++ todo_with_alarm/lib/viewmodels/goal/goal_management_viewmodel.dart
@@ -1,6 +1,7 @@
 // lib/viewmodels/goal/goal_management_viewmodel.dart
 
 import 'package:flutter/material.dart';
+import 'package:todo_with_alarm/models/goal_status.dart';
 import '../../models/goal.dart';
 import '../../services/goal_service.dart';
 
@@ -9,6 +10,7 @@ class GoalManagementViewModel extends ChangeNotifier {
   List<Goal> allGoals = [];
   List<Goal> filteredGoals = [];
   String filter = '전체';
+  // 서브 필터 없이 '완료' 탭에서만 두 가지 상태를 표시하도록 수정
 
   GoalManagementViewModel(this.goalService) {
     loadGoals();
@@ -29,7 +31,10 @@ class GoalManagementViewModel extends ChangeNotifier {
     if (filter == '전체') {
       filteredGoals = allGoals;
     } else if (filter == '완료') {
-      filteredGoals = allGoals.where((goal) => goal.isCompleted).toList();
+      // '완료' 탭에서는 status가 completed 또는 givenUp인 목표만 필터링
+      filteredGoals = allGoals.where((goal) =>
+          goal.status == GoalStatus.completed ||
+          goal.status == GoalStatus.givenUp).toList();
     }
     notifyListeners();
   }
@@ -47,6 +52,7 @@ class GoalManagementViewModel extends ChangeNotifier {
     await goalService.giveUpGoal(goalId);
     await loadGoals(); // 목표 목록 다시 로드
   }
+
   Future<void> deleteGoal(String goalId) async {
     await goalService.deleteGoal(goalId);
     await loadGoals(); // 목표 목록 다시 로드
