11fad71b0ce90dc84e65ab5435dda195cc938fc4
SeoJun <jun@SeoJunui-MacBookPro.local>
2025-02-12 20:53:15 +0900

    del : 불필요 파일 삭제

--- todo_with_alarm/test/mocks/mock_build_context.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:mockito/annotations.dart';
-import 'package:mockito/mockito.dart';
-
-// `BuildContext`와 관련된 위젯을 모킹합니다.
-@GenerateMocks([BuildContext, NavigatorState, ScaffoldMessengerState])
-void main() {}
