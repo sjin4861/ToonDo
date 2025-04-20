a32734b73484fe33f738ce31c14dc87953a90645
SeoJun <jun@SeoJunui-MacBookPro.local>
2025-04-20 13:49:56 +0900

    refactor: Update GoalRepository interface with local and remote operations

--- toondo/packages/domain/lib/repositories/goal_repository.dart
+++ toondo/packages/domain/lib/repositories/goal_repository.dart
@@ -2,11 +2,18 @@ import 'package:domain/entities/goal.dart';
 import 'package:domain/entities/status.dart';
 
 abstract class GoalRepository {
-  Future<List<Goal>> readGoals();
-  Future<Goal> createGoal(Goal goal);
-  Future<void> updateGoal(Goal goal);
-  Future<void> deleteGoal(String goalId);
-  List<Goal> getLocalGoals();
-  Future<bool> updateGoalStatus(Goal goal, Status newStatus);
-  Future<bool> updateGoalProgress(Goal goal, double newProgress);
+  // 로컬 전용 CRUD
+  List<Goal> getGoalsLocal();
+  Future<Goal?> getGoalByIdLocal(String goalId);
+  Future<void> saveGoalLocal(Goal goal);
+  Future<void> updateGoalLocal(Goal goal);
+  Future<void> deleteGoalLocal(String goalId);
+
+  // 원격 전용 CRUD
+  Future<List<Goal>> fetchGoalsRemote();
+  Future<Goal> createGoalRemote(Goal goal);
+  Future<bool> updateGoalStatusRemote(Goal goal, Status newStatus);
+  Future<bool> updateGoalProgressRemote(Goal goal, double newProgress);
+  Future<void> updateGoalRemote(Goal goal);
+  Future<void> deleteGoalRemote(String goalId);
 }
