eb675357d83ea80d356ebb105d11e8aee06c790b
SeoJun <jun@SeoJunui-MacBookPro.local>
2025-05-11 16:12:46 +0900

    add : Message 감지 유스 케이스 추가

---
+++ toondo/packages/domain/lib/usecases/character/slime_on_massage.dart
@@ -0,0 +1,19 @@
+import 'package:domain/entities/gesture.dart';
+import 'package:domain/entities/slime_response.dart';
+import 'package:domain/entities/goal.dart';
+import 'package:domain/entities/todo.dart';
+import 'package:domain/repositories/slime_repository.dart';
+import 'package:injectable/injectable.dart';
+
+@injectable
+class SlimeOnMessageUseCase {
+  final SlimeRepository _repo;
+  SlimeOnMessageUseCase(this._repo);
+
+  Future<SlimeResponse> call({
+    required String text,
+    List<Goal> goals = const [],
+    List<Todo> todos = const [],
+  }) =>
+      _repo.processMessage(text: text, goals: goals, todos: todos);
+}
