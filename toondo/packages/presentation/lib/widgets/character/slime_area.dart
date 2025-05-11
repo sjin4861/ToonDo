6b46ab0fe533ce5eb0c07e97cce8f0800d74794c
SeoJun <jun@SeoJunui-MacBookPro.local>
2025-05-11 23:03:01 +0900

    fix : slime area 의존성 간소화, 책임 분리

--- toondo/packages/presentation/lib/widgets/character/slime_area.dart
+++ toondo/packages/presentation/lib/widgets/character/slime_area.dart
@@ -1,40 +1,47 @@
 import 'dart:ui';
 import 'package:common/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:presentation/viewmodels/character/slime_character_vm.dart';
 import 'package:presentation/widgets/character/slime_character_widget.dart';
+import 'package:provider/provider.dart';
 
 class SlimeArea extends StatelessWidget {
   const SlimeArea({super.key});
 
   @override
   Widget build(BuildContext context) {
-    const double slimeWidth  = 160;   // 실제 Rive 기준
-    const double shadowScale = 1.05;  // Rive보다 살짝 넓게
-    const double shadowDY    = 28;    // 슬라임 아랫부분 여백
+    return ChangeNotifierProvider(
+      create: (_) => GetIt.I<SlimeCharacterViewModel>(),
+      child: const _SlimeStack(),
+    );
+  }
+}
+
+class _SlimeStack extends StatelessWidget {
+  const _SlimeStack();
+
+  @override
+  Widget build(BuildContext context) {
+    const w = 300.0, shadowDy = 25.0, shadowScale = 1.0;
 
     return SizedBox(
-      width: slimeWidth,
-      height: slimeWidth + 40, // 그림자 포함 높이
+      width: w,
+      height: w + 50,
       child: Stack(
         alignment: Alignment.center,
         children: [
-          // ① 그림자
           Transform.translate(
-            offset: const Offset(0, shadowDY),
+            offset: const Offset(0, shadowDy),
             child: Transform.scale(
               scale: shadowScale,
-              child: Assets.images.imgHomeShadowPng.image(
-                width: slimeWidth,
-                fit: BoxFit.contain,
-              ),
+              child: Assets.images.imgHomeShadowPng.image(width: w),
             ),
           ),
-
-          // ② 슬라임 본체
-          SlimeCharacterWidget(
-            initialAnimationName: 'idle',
+          const SlimeCharacterWidget(
             enableGestures: true,
-            showDebugInfo: true, // 제스처 디버그 정보 표시
+            showDebugInfo: true, 
+            initialAnimationName: 'idle',
           ),
         ],
       ),
