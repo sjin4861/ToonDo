24a6acfbc388af3102c096758280613ec902c0f3
aengzu <jeon7879@naver.com>
2025-06-13 12:09:49 +0900

    refactor(goal): goal selector 바텀시트 수정

--- toondo/packages/presentation/lib/widgets/goal/input/goal_icon_bottom_sheet.dart
+++ toondo/packages/presentation/lib/widgets/goal/input/goal_icon_bottom_sheet.dart
@@ -6,27 +6,27 @@ class GoalIconBottomSheet extends StatelessWidget {
     '수업/과제': [
       'assets/icons/ic_100point.svg',
       'assets/icons/ic_poo.svg',
-      'assets/icons/ic_bomb.svg'
+      'assets/icons/ic_bomb.svg',
     ],
     '공부': [
       'assets/icons/ic_100point.svg',
       'assets/icons/ic_100point.svg',
-      'assets/icons/ic_100point.svg'
+      'assets/icons/ic_100point.svg',
     ],
     '운동/건강': [
       'assets/icons/ic_100point.svg',
       'assets/icons/ic_100point.svg',
-      'assets/icons/ic_100point.svg'
+      'assets/icons/ic_100point.svg',
     ],
     '취미/자기계발': [
       'assets/icons/ic_100point.svg',
       'assets/icons/ic_100point.svg',
-      'assets/icons/ic_100point.svg'
+      'assets/icons/ic_100point.svg',
     ],
     '기타': [
       'assets/icons/ic_100point.svg',
       'assets/icons/ic_100point.svg',
-      'assets/icons/ic_100point.svg'
+      'assets/icons/ic_100point.svg',
     ],
   };
 
@@ -64,55 +64,75 @@ class GoalIconBottomSheet extends StatelessWidget {
               padding: EdgeInsets.symmetric(horizontal: 28),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: iconCategories.entries.map((entry) {
-                  String category = entry.key;
-                  List<String> icons = entry.value;
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        category,
-                        style: TextStyle(
-                          color: Color(0xFF1C1D1B),
-                          fontSize: 10,
-                          fontFamily: 'Pretendard Variable',
-                          fontWeight: FontWeight.w400,
-                          letterSpacing: 0.15,
-                        ),
-                      ),
-                      SizedBox(height: 8),
-                      Wrap(
-                        spacing: 16,
-                        runSpacing: 16,
-                        children: icons.map((iconPath) {
-                          return GestureDetector(
-                            onTap: () {
-                              Navigator.pop(context, iconPath);
-                            },
-                            child: Container(
-                              width: 40,
-                              height: 40,
-                              padding: const EdgeInsets.all(4),
-                              decoration: ShapeDecoration(
-                                shape: RoundedRectangleBorder(
-                                  side: BorderSide(
-                                      width: 1, color: Color(0xFFDDDDDD)),
-                                  borderRadius: BorderRadius.circular(1000),
+                children:
+                    iconCategories.entries.map((entry) {
+                      String category = entry.key;
+                      List<String> icons = entry.value;
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                category,
+                                style: const TextStyle(
+                                  color: Color(0xFF1C1D1B),
+                                  fontSize: 10,
+                                  fontFamily: 'Pretendard Variable',
+                                  fontWeight: FontWeight.w400,
+                                  letterSpacing: 0.15,
                                 ),
                               ),
-                              child: SvgPicture.asset( // SVG 렌더링으로 변경
-                                iconPath,
-                                width: 24,
-                                height: 24,
+                              const SizedBox(height: 8),
+                              GridView.count(
+                                crossAxisCount: 6,
+                                shrinkWrap: true,
+                                physics: const NeverScrollableScrollPhysics(),
+                                crossAxisSpacing: 12,
+                                mainAxisSpacing: 12,
+                                children: List.generate(6, (index) {
+                                  if (index < icons.length) {
+                                    final iconPath = icons[index];
+                                    return GestureDetector(
+                                      onTap:
+                                          () =>
+                                              Navigator.pop(context, iconPath),
+                                      child: Container(
+                                        padding: const EdgeInsets.all(4),
+                                        decoration: BoxDecoration(
+                                          shape: BoxShape.circle,
+                                          border: Border.all(
+                                            color: const Color(0xFFDDDDDD),
+                                            width: 1,
+                                          ),
+                                        ),
+                                        child: SvgPicture.asset(
+                                          iconPath,
+                                          width: 24,
+                                          height: 24,
+                                        ),
+                                      ),
+                                    );
+                                  } else {
+                                    // 빈 아이템 (터치 불가)
+                                    return Container(
+                                      decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        border: Border.all(
+                                          color: const Color(0xFFDDDDDD),
+                                          width: 1,
+                                        ),
+                                      ),
+                                    );
+                                  }
+                                }),
                               ),
-                            ),
-                          );
-                        }).toList(),
-                      ),
-                      SizedBox(height: 24),
-                    ],
-                  );
-                }).toList(),
+                            ],
+                          ),
+                        ],
+                      );
+                    }).toList(),
               ),
             ),
           ),
