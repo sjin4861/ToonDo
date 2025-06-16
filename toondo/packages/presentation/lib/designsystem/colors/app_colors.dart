5f3ba720f72e4cb7f05f95686b93ff21439c7069
aengzu <jeon7879@naver.com>
2025-06-17 01:35:32 +0900

    feat: add app_colors.dart for design system color constants

---
+++ toondo/packages/presentation/lib/designsystem/colors/app_colors.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Status
+  static const Color status100 = Color(0xFF1C1D1B);
+  static const Color status0 = Color(0xFFFFFFFF);
+
+  // Yellow
+  static const Color yellow100 = Color(0xFFFDF6CE);
+  static const Color yellow200 = Color(0xFFFDF0A0);
+  static const Color yellow300 = Color(0xFFFDEB6F);
+  static const Color yellow400 = Color(0xFFFCE64C);
+  static const Color yellow500 = Color(0xFFFEDA44);
+  static const Color yellow600 = Color(0xFFDBC546);
+  static const Color yellow700 = Color(0xFFEEECCF);
+  static const Color yellow800 = Color(0xFF8E822C);
+  static const Color yellow900 = Color(0xFF5E5418);
+
+  // Green
+  static const Color green100 = Color(0xFFE8F2DD);
+  static const Color green200 = Color(0xFFD4E7BA);
+  static const Color green300 = Color(0xFFB3D88F);
+  static const Color green400 = Color(0xFF90C85F);
+  static const Color green500 = Color(0xFF72B84A);
+  static const Color green600 = Color(0xFF548734);
+  static const Color green700 = Color(0xFF3D5D26);
+  static const Color green800 = Color(0xFF263B1A);
+  static const Color green900 = Color(0xFF1A2812);
+
+  // Blue
+  static const Color blue100 = Color(0xFFE3F4FD);
+  static const Color blue200 = Color(0xFFC8E8FA);
+  static const Color blue300 = Color(0xFFA8D9F8);
+  static const Color blue400 = Color(0xFF86CAF5);
+  static const Color blue500 = Color(0xFF66BAF3);
+  static const Color blue600 = Color(0xFF4189C0);
+  static const Color blue700 = Color(0xFF285F8A);
+  static const Color blue800 = Color(0xFF173A4F);
+  static const Color blue900 = Color(0xFF102733);
+
+  // Red
+  static const Color red100 = Color(0xFFFDE9EA);
+  static const Color red200 = Color(0xFFFBD2D3);
+  static const Color red300 = Color(0xFFF8B4B5);
+  static const Color red400 = Color(0xFFF59293);
+  static const Color red500 = Color(0xFFF26E71);
+  static const Color red600 = Color(0xFFD65255);
+  static const Color red700 = Color(0xFFAE3F43);
+  static const Color red800 = Color(0xFF802C30);
+  static const Color red900 = Color(0xFF582123);
+
+  // Lime
+  static const Color lime100 = Color(0xFFF4F7D6);
+  static const Color lime200 = Color(0xFFEAF2B2);
+  static const Color lime300 = Color(0xFFDFEC8D);
+  static const Color lime400 = Color(0xFFD5E669);
+  static const Color lime500 = Color(0xFFC1D453);
+  static const Color lime600 = Color(0xFFA0AD49);
+  static const Color lime700 = Color(0xFF7F873F);
+  static const Color lime800 = Color(0xFF5D6134);
+  static const Color lime900 = Color(0xFF3C3B2A);
+
+  // Brown
+  static const Color brown100 = Color(0xFFEDEBE8);
+  static const Color brown200 = Color(0xFFD6D2CD);
+  static const Color brown300 = Color(0xFFB8B1A9);
+  static const Color brown400 = Color(0xFF998F85);
+  static const Color brown500 = Color(0xFF7A6E61);
+  static const Color brown600 = Color(0xFF5C4D40);
+  static const Color brown700 = Color(0xFF3E2D1F);
+  static const Color brown800 = Color(0xFF231A13);
+  static const Color brown900 = Color(0xFF1A120D);
+
+  // Orange
+  static const Color orange100 = Color(0xFFFFEEE7);
+  static const Color orange200 = Color(0xFFFFD7C2);
+  static const Color orange300 = Color(0xFFFFB781);
+  static const Color orange400 = Color(0xFFFF9640);
+  static const Color orange500 = Color(0xFFF3761B);
+  static const Color orange600 = Color(0xFFD7600D);
+  static const Color orange700 = Color(0xFFB94D00);
+  static const Color orange800 = Color(0xFF8C3900);
+  static const Color orange900 = Color(0xFF612600);
+}
