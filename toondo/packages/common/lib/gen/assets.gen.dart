188ec7173eb21cbee1bedc9ef84aee4150099818
aengzu <jeon7879@naver.com>
2025-06-17 19:36:55 +0900

    feat: 피그마 아이콘 파일들 업로드

--- toondo/packages/common/lib/gen/assets.gen.dart
+++ toondo/packages/common/lib/gen/assets.gen.dart
@@ -30,19 +30,111 @@ class $AssetsIconsGen {
   SvgGenImage get ic100point =>
       const SvgGenImage('assets/icons/ic_100point.svg');
 
+  /// File path: assets/icons/ic_activity.svg
+  SvgGenImage get icActivity =>
+      const SvgGenImage('assets/icons/ic_activity.svg');
+
   /// File path: assets/icons/ic_ai_analysis.svg
   SvgGenImage get icAiAnalysis =>
       const SvgGenImage('assets/icons/ic_ai_analysis.svg');
 
+  /// File path: assets/icons/ic_airplay.svg
+  SvgGenImage get icAirplay => const SvgGenImage('assets/icons/ic_airplay.svg');
+
+  /// File path: assets/icons/ic_alert-circle.svg
+  SvgGenImage get icAlertCircle =>
+      const SvgGenImage('assets/icons/ic_alert-circle.svg');
+
+  /// File path: assets/icons/ic_alert-triangle.svg
+  SvgGenImage get icAlertTriangle =>
+      const SvgGenImage('assets/icons/ic_alert-triangle.svg');
+
   /// File path: assets/icons/ic_alien.svg
   SvgGenImage get icAlien => const SvgGenImage('assets/icons/ic_alien.svg');
+
+  /// File path: assets/icons/ic_award.svg
+  SvgGenImage get icAward => const SvgGenImage('assets/icons/ic_award.svg');
 
   /// File path: assets/icons/ic_bomb.svg
   SvgGenImage get icBomb => const SvgGenImage('assets/icons/ic_bomb.svg');
 
+  /// File path: assets/icons/ic_book-open.svg
+  SvgGenImage get icBookOpen =>
+      const SvgGenImage('assets/icons/ic_book-open.svg');
+
+  /// File path: assets/icons/ic_book.svg
+  SvgGenImage get icBook => const SvgGenImage('assets/icons/ic_book.svg');
+
+  /// File path: assets/icons/ic_bookmark.svg
+  SvgGenImage get icBookmark =>
+      const SvgGenImage('assets/icons/ic_bookmark.svg');
+
   /// File path: assets/icons/ic_calendar.svg
   SvgGenImage get icCalendar =>
       const SvgGenImage('assets/icons/ic_calendar.svg');
+
+  /// File path: assets/icons/ic_camera.svg
+  SvgGenImage get icCamera => const SvgGenImage('assets/icons/ic_camera.svg');
+
+  /// File path: assets/icons/ic_check-circle.svg
+  SvgGenImage get icCheckCircle =>
+      const SvgGenImage('assets/icons/ic_check-circle.svg');
+
+  /// File path: assets/icons/ic_check.svg
+  SvgGenImage get icCheck => const SvgGenImage('assets/icons/ic_check.svg');
+
+  /// File path: assets/icons/ic_chevron-down.svg
+  SvgGenImage get icChevronDown =>
+      const SvgGenImage('assets/icons/ic_chevron-down.svg');
+
+  /// File path: assets/icons/ic_chevron-left.svg
+  SvgGenImage get icChevronLeft =>
+      const SvgGenImage('assets/icons/ic_chevron-left.svg');
+
+  /// File path: assets/icons/ic_chevron-right.svg
+  SvgGenImage get icChevronRight =>
+      const SvgGenImage('assets/icons/ic_chevron-right.svg');
+
+  /// File path: assets/icons/ic_chevron-up.svg
+  SvgGenImage get icChevronUp =>
+      const SvgGenImage('assets/icons/ic_chevron-up.svg');
+
+  /// File path: assets/icons/ic_clipboard.svg
+  SvgGenImage get icClipboard =>
+      const SvgGenImage('assets/icons/ic_clipboard.svg');
+
+  /// File path: assets/icons/ic_clock.svg
+  SvgGenImage get icClock => const SvgGenImage('assets/icons/ic_clock.svg');
+
+  /// File path: assets/icons/ic_code.svg
+  SvgGenImage get icCode => const SvgGenImage('assets/icons/ic_code.svg');
+
+  /// File path: assets/icons/ic_command.svg
+  SvgGenImage get icCommand => const SvgGenImage('assets/icons/ic_command.svg');
+
+  /// File path: assets/icons/ic_compass.svg
+  SvgGenImage get icCompass => const SvgGenImage('assets/icons/ic_compass.svg');
+
+  /// File path: assets/icons/ic_cpu.svg
+  SvgGenImage get icCpu => const SvgGenImage('assets/icons/ic_cpu.svg');
+
+  /// File path: assets/icons/ic_database.svg
+  SvgGenImage get icDatabase =>
+      const SvgGenImage('assets/icons/ic_database.svg');
+
+  /// File path: assets/icons/ic_download-cloud.svg
+  SvgGenImage get icDownloadCloud =>
+      const SvgGenImage('assets/icons/ic_download-cloud.svg');
+
+  /// File path: assets/icons/ic_download.svg
+  SvgGenImage get icDownload =>
+      const SvgGenImage('assets/icons/ic_download.svg');
+
+  /// File path: assets/icons/ic_edit-2.svg
+  SvgGenImage get icEdit2 => const SvgGenImage('assets/icons/ic_edit-2.svg');
+
+  /// File path: assets/icons/ic_edit.svg
+  SvgGenImage get icEdit => const SvgGenImage('assets/icons/ic_edit.svg');
 
   /// File path: assets/icons/ic_eisenhower.svg
   SvgGenImage get icEisenhower =>
@@ -60,31 +152,160 @@ class $AssetsIconsGen {
   /// File path: assets/icons/ic_face_3.svg
   SvgGenImage get icFace3 => const SvgGenImage('assets/icons/ic_face_3.svg');
 
+  /// File path: assets/icons/ic_figma.svg
+  SvgGenImage get icFigma => const SvgGenImage('assets/icons/ic_figma.svg');
+
+  /// File path: assets/icons/ic_file-text.svg
+  SvgGenImage get icFileText =>
+      const SvgGenImage('assets/icons/ic_file-text.svg');
+
+  /// File path: assets/icons/ic_film.svg
+  SvgGenImage get icFilm => const SvgGenImage('assets/icons/ic_film.svg');
+
+  /// File path: assets/icons/ic_filter.svg
+  SvgGenImage get icFilter => const SvgGenImage('assets/icons/ic_filter.svg');
+
+  /// File path: assets/icons/ic_flag.svg
+  SvgGenImage get icFlag => const SvgGenImage('assets/icons/ic_flag.svg');
+
+  /// File path: assets/icons/ic_folder.svg
+  SvgGenImage get icFolder => const SvgGenImage('assets/icons/ic_folder.svg');
+
+  /// File path: assets/icons/ic_frown.svg
+  SvgGenImage get icFrown => const SvgGenImage('assets/icons/ic_frown.svg');
+
+  /// File path: assets/icons/ic_gift.svg
+  SvgGenImage get icGift => const SvgGenImage('assets/icons/ic_gift.svg');
+
+  /// File path: assets/icons/ic_git-pull-request.svg
+  SvgGenImage get icGitPullRequest =>
+      const SvgGenImage('assets/icons/ic_git-pull-request.svg');
+
+  /// File path: assets/icons/ic_github.svg
+  SvgGenImage get icGithub => const SvgGenImage('assets/icons/ic_github.svg');
+
+  /// File path: assets/icons/ic_gitlab.svg
+  SvgGenImage get icGitlab => const SvgGenImage('assets/icons/ic_gitlab.svg');
+
   /// File path: assets/icons/ic_goal.svg
   SvgGenImage get icGoal => const SvgGenImage('assets/icons/ic_goal.svg');
+
+  /// File path: assets/icons/ic_headphones.svg
+  SvgGenImage get icHeadphones =>
+      const SvgGenImage('assets/icons/ic_headphones.svg');
+
+  /// File path: assets/icons/ic_heart.svg
+  SvgGenImage get icHeart => const SvgGenImage('assets/icons/ic_heart.svg');
 
   /// File path: assets/icons/ic_heart_with_arrow.svg
   SvgGenImage get icHeartWithArrow =>
       const SvgGenImage('assets/icons/ic_heart_with_arrow.svg');
 
+  /// File path: assets/icons/ic_help-circle.svg
+  SvgGenImage get icHelpCircle =>
+      const SvgGenImage('assets/icons/ic_help-circle.svg');
+
+  /// File path: assets/icons/ic_image.svg
+  SvgGenImage get icImage => const SvgGenImage('assets/icons/ic_image.svg');
+
+  /// File path: assets/icons/ic_info.svg
+  SvgGenImage get icInfo => const SvgGenImage('assets/icons/ic_info.svg');
+
+  /// File path: assets/icons/ic_layout.svg
+  SvgGenImage get icLayout => const SvgGenImage('assets/icons/ic_layout.svg');
+
+  /// File path: assets/icons/ic_link-2.svg
+  SvgGenImage get icLink2 => const SvgGenImage('assets/icons/ic_link-2.svg');
+
+  /// File path: assets/icons/ic_list.svg
+  SvgGenImage get icList => const SvgGenImage('assets/icons/ic_list.svg');
+
+  /// File path: assets/icons/ic_mail.svg
+  SvgGenImage get icMail => const SvgGenImage('assets/icons/ic_mail.svg');
+
+  /// File path: assets/icons/ic_map-pin.svg
+  SvgGenImage get icMapPin => const SvgGenImage('assets/icons/ic_map-pin.svg');
+
+  /// File path: assets/icons/ic_meh.svg
+  SvgGenImage get icMeh => const SvgGenImage('assets/icons/ic_meh.svg');
+
+  /// File path: assets/icons/ic_menu.svg
+  SvgGenImage get icMenu => const SvgGenImage('assets/icons/ic_menu.svg');
+
+  /// File path: assets/icons/ic_message-circle.svg
+  SvgGenImage get icMessageCircle =>
+      const SvgGenImage('assets/icons/ic_message-circle.svg');
+
+  /// File path: assets/icons/ic_minus-circle.svg
+  SvgGenImage get icMinusCircle =>
+      const SvgGenImage('assets/icons/ic_minus-circle.svg');
+
+  /// File path: assets/icons/ic_monitor.svg
+  SvgGenImage get icMonitor => const SvgGenImage('assets/icons/ic_monitor.svg');
+
   /// File path: assets/icons/ic_monster.svg
   SvgGenImage get icMonster => const SvgGenImage('assets/icons/ic_monster.svg');
+
+  /// File path: assets/icons/ic_moon.svg
+  SvgGenImage get icMoon => const SvgGenImage('assets/icons/ic_moon.svg');
 
   /// File path: assets/icons/ic_mypage.svg
   SvgGenImage get icMypage => const SvgGenImage('assets/icons/ic_mypage.svg');
 
+  /// File path: assets/icons/ic_pen-tool.svg
+  SvgGenImage get icPenTool =>
+      const SvgGenImage('assets/icons/ic_pen-tool.svg');
+
+  /// File path: assets/icons/ic_phone-call.svg
+  SvgGenImage get icPhoneCall =>
+      const SvgGenImage('assets/icons/ic_phone-call.svg');
+
+  /// File path: assets/icons/ic_pie-chart.svg
+  SvgGenImage get icPieChart =>
+      const SvgGenImage('assets/icons/ic_pie-chart.svg');
+
+  /// File path: assets/icons/ic_plus-circle.svg
+  SvgGenImage get icPlusCircle =>
+      const SvgGenImage('assets/icons/ic_plus-circle.svg');
+
   /// File path: assets/icons/ic_plus.svg
   SvgGenImage get icPlus => const SvgGenImage('assets/icons/ic_plus.svg');
-
-  /// File path: assets/icons/ic_plus_bold.svg
-  SvgGenImage get icPlusBold =>
-      const SvgGenImage('assets/icons/ic_plus_bold.svg');
 
   /// File path: assets/icons/ic_poo.svg
   SvgGenImage get icPoo => const SvgGenImage('assets/icons/ic_poo.svg');
 
+  /// File path: assets/icons/ic_rotate-cw.svg
+  SvgGenImage get icRotateCw =>
+      const SvgGenImage('assets/icons/ic_rotate-cw.svg');
+
+  /// File path: assets/icons/ic_search.svg
+  SvgGenImage get icSearch => const SvgGenImage('assets/icons/ic_search.svg');
+
+  /// File path: assets/icons/ic_settings.svg
+  SvgGenImage get icSettings =>
+      const SvgGenImage('assets/icons/ic_settings.svg');
+
+  /// File path: assets/icons/ic_share-2.svg
+  SvgGenImage get icShare2 => const SvgGenImage('assets/icons/ic_share-2.svg');
+
+  /// File path: assets/icons/ic_share.svg
+  SvgGenImage get icShare => const SvgGenImage('assets/icons/ic_share.svg');
+
+  /// File path: assets/icons/ic_shopping-cart.svg
+  SvgGenImage get icShoppingCart =>
+      const SvgGenImage('assets/icons/ic_shopping-cart.svg');
+
   /// File path: assets/icons/ic_skull.svg
   SvgGenImage get icSkull => const SvgGenImage('assets/icons/ic_skull.svg');
+
+  /// File path: assets/icons/ic_smile.svg
+  SvgGenImage get icSmile => const SvgGenImage('assets/icons/ic_smile.svg');
+
+  /// File path: assets/icons/ic_speaker.svg
+  SvgGenImage get icSpeaker => const SvgGenImage('assets/icons/ic_speaker.svg');
+
+  /// File path: assets/icons/ic_star.svg
+  SvgGenImage get icStar => const SvgGenImage('assets/icons/ic_star.svg');
 
   /// File path: assets/icons/ic_stats.svg
   SvgGenImage get icStats => const SvgGenImage('assets/icons/ic_stats.svg');
@@ -92,28 +313,179 @@ class $AssetsIconsGen {
   /// File path: assets/icons/ic_store.svg
   SvgGenImage get icStore => const SvgGenImage('assets/icons/ic_store.svg');
 
+  /// File path: assets/icons/ic_table.svg
+  SvgGenImage get icTable => const SvgGenImage('assets/icons/ic_table.svg');
+
+  /// File path: assets/icons/ic_tag.svg
+  SvgGenImage get icTag => const SvgGenImage('assets/icons/ic_tag.svg');
+
+  /// File path: assets/icons/ic_target.svg
+  SvgGenImage get icTarget => const SvgGenImage('assets/icons/ic_target.svg');
+
+  /// File path: assets/icons/ic_thumbs-down.svg
+  SvgGenImage get icThumbsDown =>
+      const SvgGenImage('assets/icons/ic_thumbs-down.svg');
+
+  /// File path: assets/icons/ic_thumbs-up.svg
+  SvgGenImage get icThumbsUp =>
+      const SvgGenImage('assets/icons/ic_thumbs-up.svg');
+
+  /// File path: assets/icons/ic_tool.svg
+  SvgGenImage get icTool => const SvgGenImage('assets/icons/ic_tool.svg');
+
+  /// File path: assets/icons/ic_trash-2.svg
+  SvgGenImage get icTrash2 => const SvgGenImage('assets/icons/ic_trash-2.svg');
+
+  /// File path: assets/icons/ic_trash.svg
+  SvgGenImage get icTrash => const SvgGenImage('assets/icons/ic_trash.svg');
+
+  /// File path: assets/icons/ic_unlock.svg
+  SvgGenImage get icUnlock => const SvgGenImage('assets/icons/ic_unlock.svg');
+
+  /// File path: assets/icons/ic_upload-cloud.svg
+  SvgGenImage get icUploadCloud =>
+      const SvgGenImage('assets/icons/ic_upload-cloud.svg');
+
+  /// File path: assets/icons/ic_upload.svg
+  SvgGenImage get icUpload => const SvgGenImage('assets/icons/ic_upload.svg');
+
+  /// File path: assets/icons/ic_user.svg
+  SvgGenImage get icUser => const SvgGenImage('assets/icons/ic_user.svg');
+
+  /// File path: assets/icons/ic_video.svg
+  SvgGenImage get icVideo => const SvgGenImage('assets/icons/ic_video.svg');
+
+  /// File path: assets/icons/ic_volume-2.svg
+  SvgGenImage get icVolume2 =>
+      const SvgGenImage('assets/icons/ic_volume-2.svg');
+
+  /// File path: assets/icons/ic_wifi-off.svg
+  SvgGenImage get icWifiOff =>
+      const SvgGenImage('assets/icons/ic_wifi-off.svg');
+
+  /// File path: assets/icons/ic_wifi.svg
+  SvgGenImage get icWifi => const SvgGenImage('assets/icons/ic_wifi.svg');
+
+  /// File path: assets/icons/ic_x-circle.svg
+  SvgGenImage get icXCircle =>
+      const SvgGenImage('assets/icons/ic_x-circle.svg');
+
+  /// File path: assets/icons/ic_x.svg
+  SvgGenImage get icX => const SvgGenImage('assets/icons/ic_x.svg');
+
+  /// File path: assets/icons/ic_youtube.svg
+  SvgGenImage get icYoutube => const SvgGenImage('assets/icons/ic_youtube.svg');
+
+  /// File path: assets/icons/ic_zap.svg
+  SvgGenImage get icZap => const SvgGenImage('assets/icons/ic_zap.svg');
+
   /// List of all assets
   List<SvgGenImage> get values => [
         ic100point,
+        icActivity,
         icAiAnalysis,
+        icAirplay,
+        icAlertCircle,
+        icAlertTriangle,
         icAlien,
+        icAward,
         icBomb,
+        icBookOpen,
+        icBook,
+        icBookmark,
         icCalendar,
+        icCamera,
+        icCheckCircle,
+        icCheck,
+        icChevronDown,
+        icChevronLeft,
+        icChevronRight,
+        icChevronUp,
+        icClipboard,
+        icClock,
+        icCode,
+        icCommand,
+        icCompass,
+        icCpu,
+        icDatabase,
+        icDownloadCloud,
+        icDownload,
+        icEdit2,
+        icEdit,
         icEisenhower,
         icFace0,
         icFace1,
         icFace2,
         icFace3,
+        icFigma,
+        icFileText,
+        icFilm,
+        icFilter,
+        icFlag,
+        icFolder,
+        icFrown,
+        icGift,
+        icGitPullRequest,
+        icGithub,
+        icGitlab,
         icGoal,
+        icHeadphones,
+        icHeart,
         icHeartWithArrow,
+        icHelpCircle,
+        icImage,
+        icInfo,
+        icLayout,
+        icLink2,
+        icList,
+        icMail,
+        icMapPin,
+        icMeh,
+        icMenu,
+        icMessageCircle,
+        icMinusCircle,
+        icMonitor,
         icMonster,
+        icMoon,
         icMypage,
+        icPenTool,
+        icPhoneCall,
+        icPieChart,
+        icPlusCircle,
         icPlus,
-        icPlusBold,
         icPoo,
+        icRotateCw,
+        icSearch,
+        icSettings,
+        icShare2,
+        icShare,
+        icShoppingCart,
         icSkull,
+        icSmile,
+        icSpeaker,
+        icStar,
         icStats,
-        icStore
+        icStore,
+        icTable,
+        icTag,
+        icTarget,
+        icThumbsDown,
+        icThumbsUp,
+        icTool,
+        icTrash2,
+        icTrash,
+        icUnlock,
+        icUploadCloud,
+        icUpload,
+        icUser,
+        icVideo,
+        icVolume2,
+        icWifiOff,
+        icWifi,
+        icXCircle,
+        icX,
+        icYoutube,
+        icZap
       ];
 }
 
