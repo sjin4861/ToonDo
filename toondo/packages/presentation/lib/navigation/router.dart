c803dad809f1ad34e74cb5e6cf1eac6f50d755ce
SeoJun <jun@SeoJunui-MacBookPro.local>
2025-07-16 20:31:01 +0900

    refactor(presentation): Disable slime chat route

--- toondo/packages/presentation/lib/navigation/router.dart
+++ toondo/packages/presentation/lib/navigation/router.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:presentation/viewmodels/character/chat_viewmodel.dart';
 import 'package:presentation/viewmodels/welcome/welcome_viewmodel.dart';
-import 'package:presentation/views/chat/slime_chat_page.dart';
 import 'package:presentation/views/goal/input/goal_input_screen.dart';
 import 'package:presentation/views/mypage/account_setting/account_setting_screen.dart';
 import 'package:presentation/views/mypage/account_setting/nickname_change_screen.dart';
@@ -60,13 +58,14 @@ class AppRouter {
         return MaterialPageRoute(builder: (_) => PhoneNumberChangeScreen());
       case RoutePaths.accountSettingPasswordChange:
         return MaterialPageRoute(builder: (_) => PasswordChangeScreen());
-      case RoutePaths.slimeChat:
-        return MaterialPageRoute(
-          builder: (_) => ChangeNotifierProvider<ChatViewModel>.value(
-            value: GetIt.instance<ChatViewModel>(),
-            child: const SlimeChatPage(),
-          ),
-        );
+      // GPT 대화 기능은 추후 개발 예정으로 비활성화
+      // case RoutePaths.slimeChat:
+      //   return MaterialPageRoute(
+      //     builder: (_) => ChangeNotifierProvider<ChatViewModel>.value(
+      //       value: GetIt.instance<ChatViewModel>(),
+      //       child: const SlimeChatPage(),
+      //     ),
+      //   );
       default:
         return MaterialPageRoute(
           builder:
