0376e97d3b8232769db4ef82d99b929de08138ac
SeoJun <jun@SeoJunui-MacBookPro.local>
2025-07-16 20:30:10 +0900

    feat(domain): Add updatePhoneNumber to UserRepository interface

--- toondo/packages/domain/lib/repositories/user_repository.dart
+++ toondo/packages/domain/lib/repositories/user_repository.dart
@@ -2,6 +2,7 @@ import 'package:domain/entities/user.dart';
 
 abstract class UserRepository {
   Future<User> updateNickName(String newNickName);
+  Future<User> updatePhoneNumber(String newPhoneNumber);
   Future<User> updateUserPoints(int delta);
   Future<String?> getUserNickname();
   Future<User> getUser();
