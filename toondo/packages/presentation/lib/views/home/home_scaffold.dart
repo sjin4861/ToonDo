8c9c2bf2937f81d63f6bb4a8a21f1c169873ea69
SeoJun <jun@SeoJunui-MacBookPro.local>
2025-07-16 20:33:34 +0900

    refactor(presentation): Simplify HomeScaffold and remove chat dependency

--- toondo/packages/presentation/lib/views/home/home_scaffold.dart
+++ toondo/packages/presentation/lib/views/home/home_scaffold.dart
@@ -2,21 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:presentation/views/home/home_body.dart';
 import 'package:presentation/widgets/home/home_app_bar.dart';
 import 'package:presentation/widgets/navigation/bottom_navigation_bar_widget.dart';
-import 'package:provider/provider.dart';
-import 'package:presentation/viewmodels/home/home_viewmodel.dart';
 
 class HomeScaffold extends StatelessWidget {
-  final bool isNewLogin;
-  const HomeScaffold({super.key, required this.isNewLogin});
+  const HomeScaffold({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final homeVM = context.watch<HomeViewModel>();
-
     return Scaffold(
       extendBody: true,
       backgroundColor: Colors.transparent,
-      appBar: HomeAppBar(chatEnabled: homeVM.chatEnabled),
+      appBar: const HomeAppBar(),
       body: const HomeBody(),
       bottomNavigationBar: const BottomNavigationBarWidget(),
     );
