bd3687694067065d91de9a011ae679dd3e7c96db
SeoJun <jun@SeoJunui-MacBookPro.local>
2025-08-02 22:57:38 +0900

    refactor(user): Remove points and phone number from User entity
    Removes the  and  fields from the  entity, as they are no longer part of the user data model. This change simplifies the entity and aligns it with the recent backend changes.

--- toondo/packages/domain/lib/entities/user.dart
+++ toondo/packages/domain/lib/entities/user.dart
@@ -2,16 +2,12 @@ class User {
   final int id;
   final String loginId;
   final String? nickname;
-  final String? phoneNumber; // NOTE 현재 서버측 수정이 안되어서 nullable 인데 추후수정
-  final int points;
   final DateTime createdAt;
 
   const User._internal({
     required this.id,
     required this.loginId,
     this.nickname,
-    this.phoneNumber,
-    required this.points,
     required this.createdAt,
   });
 
@@ -19,17 +15,13 @@ class User {
     required int id,
     required String loginId,
     String? nickname,
-    required int points,
     DateTime? createdAt,
-    String? phoneNumber,
   }) {
     return User._internal(
       id: id,
       loginId: loginId,
       nickname: nickname,
-      points: points,
       createdAt: createdAt ?? DateTime.now(), // 기본값 처리
-      phoneNumber: phoneNumber,
     );
   }
 }
