ba519a15777aaec2ea50e9aade1c62a7f350e561
sjin4861 <sjin4861@pusan.ac.kr>
2025-08-08 23:30:29 +0900

    TODO: Todo 엔티티 변경사항 반영 필요 - get_todo_border_color.dart
    - importance, urgency 필드 기반 로직을 eisenhower 필드 기반으로 변경 필요
    - getBorderColor 함수에서 아이젠하워 매트릭스 기반 색상 결정 로직 구현 필요

--- toondo/packages/presentation/lib/utils/get_todo_border_color.dart
+++ toondo/packages/presentation/lib/utils/get_todo_border_color.dart
@@ -3,14 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:domain/entities/todo.dart';
 import 'package:presentation/designsystem/colors/app_colors.dart';
 
+// TODO: importance, urgency 필드 기반 로직을 eisenhower 필드 기반으로 변경 필요
 Color getBorderColor(Todo todo) {
-    if (todo.importance == 1 && todo.urgency == 1) {
-      return AppColors.red300;
-    } else if (todo.importance == 1 && todo.urgency == 0) {
-      return AppColors.blue300;
-    } else if (todo.importance == 0 && todo.urgency == 1) {
-      return AppColors.yellow300;
-    } else {
-      return AppColors.brown300;
-    }
+  if (todo.importance == 1 && todo.urgency == 1) {
+    return AppColors.red300;
+  } else if (todo.importance == 1 && todo.urgency == 0) {
+    return AppColors.blue300;
+  } else if (todo.importance == 0 && todo.urgency == 1) {
+    return AppColors.yellow300;
+  } else {
+    return AppColors.brown300;
   }
+}
