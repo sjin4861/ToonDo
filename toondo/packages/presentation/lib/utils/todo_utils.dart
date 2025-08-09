57c01e675323960ddaec47cd96db2d71824181cf
SeoJun <jun@SeoJunui-MacBookPro.local>
2025-08-09 19:02:30 +0900

    refactor(presentation): Todo 관련 유틸리티 함수 수정

--- toondo/packages/presentation/lib/utils/todo_utils.dart
+++ toondo/packages/presentation/lib/utils/todo_utils.dart
@@ -7,15 +7,13 @@ extension TodoFilterOptionX on TodoFilterOption {
         return 0;
       case TodoFilterOption.goal:
         return 1;
-      // TODO: importance 필터를 eisenhower 기반 필터로 변경 필요
+      // eisenhower 기반 중요도 필터
       case TodoFilterOption.importance:
         return 2;
       case TodoFilterOption.dDay:
-        // TODO: Handle this case.
-        throw UnimplementedError();
+        return 3;
       case TodoFilterOption.daily:
-        // TODO: Handle this case.
-        throw UnimplementedError();
+        return 4;
     }
   }
 }
@@ -27,9 +25,13 @@ extension IntX on int {
         return TodoFilterOption.all;
       case 1:
         return TodoFilterOption.goal;
-      // TODO: importance 필터를 eisenhower 기반 필터로 변경 필요
+      // eisenhower 기반 중요도 필터
       case 2:
         return TodoFilterOption.importance;
+      case 3:
+        return TodoFilterOption.dDay;
+      case 4:
+        return TodoFilterOption.daily;
       default:
         return TodoFilterOption.all;
     }
