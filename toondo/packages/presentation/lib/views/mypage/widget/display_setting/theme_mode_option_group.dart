7990a9b97334cc06a305b1ab5eb1211a011e34bd
aengzu <jeon7879@naver.com>
2025-09-04 22:12:23 +0900

    refactor: 다크모드 클릭 시 준비중입니다

--- toondo/packages/presentation/lib/views/mypage/widget/display_setting/theme_mode_option_group.dart
+++ toondo/packages/presentation/lib/views/mypage/widget/display_setting/theme_mode_option_group.dart
@@ -1,9 +1,9 @@
 import 'package:domain/entities/theme_mode_type.dart';
 import 'package:flutter/material.dart';
+import 'package:presentation/designsystem/components/dialogs/comong_soon_dialog.dart';
 import 'package:presentation/designsystem/components/setting/app_theme_radio_button.dart';
 import 'package:presentation/viewmodels/my_page/display_setting/display_setting_viewmodel.dart';
 import 'package:provider/provider.dart';
-
 
 class ThemeModeOptionGroup extends StatelessWidget {
   const ThemeModeOptionGroup({super.key});
@@ -30,7 +30,9 @@ class ThemeModeOptionGroup extends StatelessWidget {
         AppThemeRadioButton(
           type: ThemeModeType.dark,
           isSelected: selectedMode == ThemeModeType.dark,
-          onTap: () => vm.selectMode(ThemeModeType.dark),
+          // TODO: 다크모드 기능 구현 후 활성화
+          // onTap: () => vm.selectMode(ThemeModeType.dark),
+          onTap: () => ComingSoonDialog.show(context),
         ),
       ],
     );
