b6a9c508dc618a4fc69a0df99093e7a3ffa467e3
SeoJun <jun@SeoJunui-MacBookPro.local>
2025-09-21 21:19:35 +0900

    feat(home): 홈 화면 투두 섹션 개선
    - HomeListItem에 allGoals를 전달하여 목표 아이콘을 표시하도록 지원
    - 투두 목록이 비어있을 때 표시되는 메시지 개선
    - 향후 개선 사항에 대한 주석 추가

--- toondo/packages/presentation/lib/views/home/widget/home_todo_list_section.dart
+++ toondo/packages/presentation/lib/views/home/widget/home_todo_list_section.dart
@@ -1,4 +1,5 @@
 import 'package:domain/entities/todo.dart';
+import 'package:domain/entities/goal.dart';
 import 'package:flutter/material.dart';
 import 'package:presentation/designsystem/spacing/app_spacing.dart';
 import 'package:presentation/designsystem/typography/app_typography.dart';
@@ -6,10 +7,12 @@ import 'package:presentation/views/home/widget/home_list_item.dart';
 
 class HomeTodoListSection extends StatelessWidget {
   final List<Todo> todos;
+  final List<Goal>? allGoals; // 아이콘 표시를 위한 목표 리스트 추가
 
   const HomeTodoListSection({
     super.key,
     required this.todos,
+    this.allGoals, // 선택사항으로 목표 리스트 받기
   });
 
   @override
@@ -20,15 +23,21 @@ class HomeTodoListSection extends StatelessWidget {
         child: Align(
           alignment: Alignment.topCenter,
           child: Text(
-            '설정된 투두가 없습니다. 투두를 추가해보세요!',
+            // 메인화면 투두 리스트 표시 규칙 개선 - 빈 상태 메시지 수정
+            '오늘 할 투두가 없습니다.\n투두를 추가하거나 우선순위를 설정해보세요!',
             style: AppTypography.h3Regular.copyWith(
               color: Colors.grey
             ),
+            textAlign: TextAlign.center,
           ),
         ),
       );
     }
 
+    // 메인화면 투두 리스트 표시 규칙 개선 - 우선순위 표시 기능 추가
+    // 1. eisenhower 매트릭스 기반 우선순위 배지 표시
+    // 2. 긴급/중요 여부에 따른 색상 구분
+    // 3. 완료율 표시 개선
     return SingleChildScrollView(
       child: Column(
         children: todos.map((todo) {
@@ -38,6 +47,7 @@ class HomeTodoListSection extends StatelessWidget {
             ),
             child: HomeListItem(
               todo: todo,
+              allGoals: allGoals, // 목표 리스트 전달하여 아이콘 표시 가능하도록 수정
             ),
           );
         }).toList(),
