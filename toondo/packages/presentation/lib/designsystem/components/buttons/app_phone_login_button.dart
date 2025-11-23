324fdd5beb9696ca3024451094a37dee70619400
SeoJun <jun@SeoJunui-MacBookPro.local>
2025-11-23 19:04:04 +0900

    fix(auth): 로그인 버튼 문구 수정

--- toondo/packages/presentation/lib/designsystem/components/buttons/app_phone_login_button.dart
+++ toondo/packages/presentation/lib/designsystem/components/buttons/app_phone_login_button.dart
@@ -36,7 +36,7 @@ class AppPhoneLoginButton extends StatelessWidget {
           ),
           SizedBox(width: AppSpacing.h12),
           Text(
-            '번호로 계속하기',
+            '아이디로 계속하기',
             textAlign: TextAlign.center,
             style: AppTypography.h3Bold.copyWith(color: AppColors.status100),
           ),
