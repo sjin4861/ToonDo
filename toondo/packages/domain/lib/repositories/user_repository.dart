4949f1b0fe2cb03de4770864fc6da4e9e6de82c3
sjin4861 <sjin4861@postech.ac.kr>
2026-01-09 01:05:41 +0900

    feat(domain): update UserRepository interface

--- toondo/packages/domain/lib/repositories/user_repository.dart
+++ toondo/packages/domain/lib/repositories/user_repository.dart
@@ -4,5 +4,6 @@ abstract class UserRepository {
   Future<User> updateNickName(String newNickName);
   Future<String?> getUserNickname();
   Future<User> getUser();
+  Future<void> updatePassword(String newPassword);
   Future<void> deleteAccount();
 }
